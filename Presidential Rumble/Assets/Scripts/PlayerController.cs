93ff55cb0b81dca5e0702bfc48dfc419bde15593
Mike <fiedelmike@gmail.com>
2014-02-13 14:22:10 -0700

    Movement, jumping, bounadries
    Added player movement (including friction adjustments on the floor).
    Player jumping handled in control script. Walls added (which are
    frictionless)

--- Presidential Rumble/Assets/Scripts/PlayerController.cs
+++ Presidential Rumble/Assets/Scripts/PlayerController.cs
@@ -2,13 +2,45 @@
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
+	public bool jump, stopHorizontal;
+	public float jumpForce = 1000f;
+
+	private bool grounded = false;
+	private Transform groundCheck;
+
+	void Awake(){
+		groundCheck = transform.Find ("groundCheck");
+	}
+
+	void Update(){
+		grounded = Physics2D.Linecast (transform.position, groundCheck.position, 1 << LayerMask.NameToLayer ("Ground"));
+
+		if (Input.GetKeyUp ("a") || Input.GetKeyUp ("d"))
+			stopHorizontal = true;
+
+		if (Input.GetKeyDown ("space") && grounded) {
+			jump = true;		
+		}
+	}
 
 	// Called each update
 	void FixedUpdate(){
-		float moveHorizontal = Input.GetAxis ("Horizontal");
-		float moveVertical = Input.GetAxis ("Vertical");
+		float moveHorizontal;
 
-		Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+		if (stopHorizontal) {
+			moveHorizontal = 0f;
+			stopHorizontal = false;
+		}
+
+		else		
+			moveHorizontal = Input.GetAxis ("Horizontal");
+
+		Vector2 movement = new Vector2(moveHorizontal*50, rigidbody2D.velocity.y);
 		rigidbody2D.velocity = movement;
+
+		if (jump) {
+			rigidbody2D.AddForce(new Vector2(0f, jumpForce));	
+			jump = false;
+		}
 	}
 }

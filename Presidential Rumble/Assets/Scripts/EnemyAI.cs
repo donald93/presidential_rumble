d47cf32d498ac79b7b8e9e2c09a444ad245e8947
u0691932 <dickson.chiu8@gmail.com>
2014-02-26 17:54:29 -0700

    Started ENEMYAI
    -Started on an EnemyAI script. I need to look into more methods of
    making an enemy on a 2d plane. Further testing is needed for this.

---
+++ Presidential Rumble/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAI : MonoBehaviour {
+
+	public Transform target;
+	public int moveSpeed;
+	private Transform myTransform;
+
+	void Awake(){
+		myTransform = transform;
+	}
+	// Use this for initialization
+	void Start () {
+		GameObject go = GameObject.FindGameObjectWithTag("Player");
+		target = go.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		myTransform.position += myTransform.position * moveSpeed * Time.deltaTime;
+	}
+}

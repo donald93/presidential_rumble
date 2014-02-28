49f3bd2d442df78dee6cebd328eb05b2726fd8c8
donald93 <blargalicious@gmail.com>
2014-02-27 20:07:38 -0700

    Added Punching
    Crappy animated punching. Collisions working as well

---
+++ Presidential Rumble/Assets/Scripts/EnemyCollisions.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyCollisions : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}	
+	
+	void OnTriggerEnter2D(Collider2D collider){
+		if (collider.gameObject.tag == "Punch"){
+			Debug.Log("ouch, I've been punched");
+		}
+	}
+	
+	void OnCollisionEnter2D (Collision2D collision){
+		if (collision.collider.gameObject.tag == "Punch"){
+			Debug.Log("ouch, I've been punched");
+		}
+	}
+}
+
--- Presidential Rumble/Assets/Scripts/PlayerController.cs
+++ Presidential Rumble/Assets/Scripts/PlayerController.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
-	public bool jump, punch, goingRight, crouch;
+	public bool jump, punch, goingLeft, crouch;
 	public float jumpForce = 1000f;
 	
 	private bool grounded = false;
@@ -30,47 +30,48 @@ public class PlayerController : MonoBehaviour {
 				animator.SetBool ("Crouching", true);
 				crouch = true;
 			}
+
 			if (Input.GetKeyUp ("s")) {
 				animator.SetBool ("Crouching", false);
 				crouch = false;
 			} 
 
+
+
+			// Jump Controls
+			if (Input.GetKeyDown ("space") && grounded) {
+				jump = true;
+				animator.SetBool("Jumping", true);
+				framesSinceJump = 0;
+			}
+			
 			if (grounded && framesSinceJump > 0)
 				animator.SetBool("Jumping", false);
 
-			//Get the current state
-			AnimatorStateInfo stateInfo = animator.GetCurrentAnimatorStateInfo(0);
 
-			if (stateInfo.nameHash == Animator.StringToHash("Base Layer.Idle") || stateInfo.nameHash == Animator.StringToHash("Base Layer.Empty State")){
-				// Punch key was pushed
-				if (Input.GetKey ("f")){
-					//animator.SetBool("Punching", true);
-
-					// loop through children and enable the punch colliders
-					Transform[] allChildren = GetComponentsInChildren<Transform>();
-					foreach (Transform child in allChildren){
-						if (child.tag == "Punch")
-							child.collider2D.enabled = true;
-						Invoke ("disablePunch", 1);
-					}
-				}
-
-				//else
-					//animator.SetBool("Punching", false);
-
-				if (Input.GetKeyDown ("space") && grounded) {
-					jump = true;
-					animator.SetBool("Jumping", true);
-					framesSinceJump = 0;
+			// Punch key was pushed
+			if (Input.GetKey ("f")){
+				animator.SetBool("Punching", true);
+			
+				// loop through children and enable the punch colliders
+				Transform[] allChildren = GetComponentsInChildren<Transform>();
+				foreach (Transform child in allChildren){
+					if (child.tag == "Punch")
+						child.collider2D.enabled = true;
+					Invoke ("disablePunch", 1);
 				}
 			}
+
+				else
+					animator.SetBool("Punching", false);
+			//}
 		}
 
-		if (!goingRight && Input.GetAxis ("Horizontal") < 0 || goingRight && Input.GetAxis ("Horizontal") > 0){
+		if (!goingLeft && Input.GetAxis ("Horizontal") < 0 || goingLeft && Input.GetAxis ("Horizontal") > 0){
 			Vector2 scale = transform.localScale;
 			scale.x *= -1;
 			transform.localScale = scale;
-			goingRight = !goingRight;
+			goingLeft = !goingLeft;
 		}
 	}
 
@@ -79,10 +80,8 @@ public class PlayerController : MonoBehaviour {
 		float moveHorizontal = Input.GetAxis ("Horizontal");
 
 		//Check if crouching to slow movement
-		if (crouch) {
-
-						rigidbody2D.velocity = new Vector2 (moveHorizontal * 15, rigidbody2D.velocity.y);
-				}
+		if (crouch && grounded) 
+			rigidbody2D.velocity = new Vector2 (moveHorizontal * 15, rigidbody2D.velocity.y);
 		else
 			rigidbody2D.velocity = new Vector2 (moveHorizontal * 25, rigidbody2D.velocity.y);
 

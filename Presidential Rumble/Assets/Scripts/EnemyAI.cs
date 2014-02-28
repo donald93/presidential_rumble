708a6203fd0bc809c2b0b51471c3380e02fbb9f0
u0691932 <dickson.chiu8@gmail.com>
2014-02-27 20:26:36 -0700

    Fixed Enemy Ai
    -Enemy now stops in front of player.

--- Presidential Rumble/Assets/Scripts/EnemyAI.cs
+++ Presidential Rumble/Assets/Scripts/EnemyAI.cs
@@ -16,20 +16,28 @@ public class EnemyAI : MonoBehaviour {
 
 	private Vector2 movement;
 
+	public Transform player;
+	//private Transform myTransform;
+
+	float moveHorizontal;
+
 	void Awake(){
 		startingPos = transform.position.x;
 		endPos = startingPos + unitsToMove;
+		//myTransform = transform;
 	}
 
 	// Use this for initialization
 	void Start () {
-
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		//movement = new Vector2(speed.x * direction.x, speed.y * direction.y);
 		//counter = 1;
+
+		//This code will make the ENEMY patrol part of the level.
+		/*
 		if (moveRight) {
 			rigidbody2D.transform.position += Vector3.right * moveSpeed * Time.deltaTime;
 		}
@@ -42,11 +50,40 @@ public class EnemyAI : MonoBehaviour {
 		if (rigidbody2D.transform.position.x <= startingPos) {
 			moveRight = true;
 		}
+		*/
+		//myTransform.position += myTransform.right * moveSpeed * Time.deltaTime;
 
 	}
 
 	void FixedUpdate(){
 		//rigidbody2D.velocity = movement;
 		//rigidbody2D.transform.position;
+		//Vector2 PlayerVector = new Vector2(Vector2.MoveTowards (rigidbody2D.transform.position, target.transform.position, 25).x, 0);
+		if (Mathf.Abs(rigidbody2D.transform.position.x - player.transform.position.x) < 5)
+		{
+			moveHorizontal = 0;
+			rigidbody2D.velocity = Vector2.zero;
+			Debug.Log("too close");
+		}
+		else if (player.transform.position.x < rigidbody2D.transform.position.x) 
+		{
+			moveHorizontal = -1;
+			rigidbody2D.velocity = new Vector2 (moveHorizontal * 25, rigidbody2D.velocity.y);
+
+		}
+
+		else if (player.transform.position.x  > rigidbody2D.transform.position.x)
+		{
+			moveHorizontal = 1;
+			rigidbody2D.velocity = new Vector2 (moveHorizontal * 25, rigidbody2D.velocity.y);
+
+		}
+
+		//Check players position against enemy position
+		//if less, move to the left and edit 
+
+
+
+		//rigidbody2D.velocity = PlayerVector;
 	}
 }

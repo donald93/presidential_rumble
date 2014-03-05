603a9972687659608629237bbaaa730eebd84b18
u0641022 <ashtonmata24@gmail.com>
2014-03-04 19:46:02 -0700

    Deleted Merge Conflict Text

--- Presidential Rumble/Assets/Scripts/EnemyHealth.cs
+++ Presidential Rumble/Assets/Scripts/EnemyHealth.cs
@@ -23,29 +23,16 @@ public class EnemyHealth : MonoBehaviour
 	
 		void OnGUI ()
 		{
-<<<<<<< HEAD
-				GUI.Box (new Rect (320, 20, 125 - (MaxHealth - CurrentHealth), 12), "");
-=======
 			
 				GUI.Box (new Rect (500, 30, 125 - (MaxHealth - CurrentHealth), 20), "French Soldier");
->>>>>>> FETCH_HEAD
-		
+
 		}
 	
 		public static void DecrementHealth ()
 		{
-<<<<<<< HEAD
 				CurrentHealth -= 4;
 				if (CurrentHealth <= 0)
-=======
-<<<<<<< HEAD
-				CurrentHealth--;
-				
-=======
-				CurrentHealth -= 1;
-				if (CurrentHealth == 0)
->>>>>>> ea5b796e085f232ec7c5bd257c58a7519ba66b62
+
 						GameObject.Find ("EventListener").SendMessage ("NextScene", 3);
->>>>>>> FETCH_HEAD
 		}
 }

592d49c36b24b19c76e56713afca0abc68e61cbe
u0691932 <dickson.chiu8@gmail.com>
2014-03-04 17:20:32 -0700

    Fixed Enemy Health Bar
    The Enemy health bar is now fixed so it runs progressively down. Except
    we don’t have a check to go back to the main menu after the enemy
    health goes to ZERO and we no longer display the health number.

--- Presidential Rumble/Assets/Scripts/EnemyHealth.cs
+++ Presidential Rumble/Assets/Scripts/EnemyHealth.cs
@@ -4,8 +4,8 @@ using System.Collections;
 public class EnemyHealth : MonoBehaviour
 {
 	
-		public static int MaxHealth = 100;
-		public static int CurrentHealth = 100;
+		public static int MaxHealth = 125;
+		public static int CurrentHealth = 125;
 	
 		// Use this for initialization
 		void Start ()
@@ -21,12 +21,13 @@ public class EnemyHealth : MonoBehaviour
 	
 		void OnGUI ()
 		{
-				GUI.Box (new Rect (10, 10, Screen.width / 2 / (MaxHealth / CurrentHealth), 20), "" + CurrentHealth);
+				GUI.Box (new Rect (320, 20, 125 - (MaxHealth - CurrentHealth), 12), "");
 		
 		}
 	
 		public static void DecrementHealth ()
 		{
 				CurrentHealth--;
+				
 		}
 }

41cb8ea3060b55080faa108599cac033dca82981
ColonelWittyHill <ctlillywhite@msn.com>
2014-03-18 20:12:46 -0600

    Level labels kind of work

--- Presidential Rumble/Assets/Scripts/Map.cs
+++ Presidential Rumble/Assets/Scripts/Map.cs
@@ -12,113 +12,141 @@ using System.Linq;
 /// </summary>
 public class Map : MonoBehaviour
 {
-		private GameObject[] buttons;
-		private GameObject startButton;
-		private int selected;
-		private bool axisBusy;
-		private string levelName;
-		private AudioSource mapAudio;
+	public GameObject[] buttons;
+	private int selected;
+	private bool axisBusy;
+	private string levelName;
+	private AudioSource mapAudio;
 
-		public Font font;
-		public Directions anchor;
-		public Texture2D guiImage;
-		public GUIButton startButton2;
-		public GUIButton backButton;
-		public AudioClip sound;
+	public Font font;
+	public Texture2D guiImage;
+	public GUIButton startButton;
+	public GUIButton backButton;
+	public AudioClip sound;
 
-		void Awake ()
+	void Awake ()
+	{
+		axisBusy = false;
+		selected = 0;
+
+		foreach (GameObject b in buttons)
 		{
-				axisBusy = false;
-				selected = 0;
-				buttons = GameObject.FindGameObjectsWithTag ("MapButtonTag").OrderBy (
-			button => button.transform.gameObject.GetComponent<MapButton> ().Scene).ToArray ();
-				startButton = GameObject.FindWithTag ("StartButtonTag");
-
-				mapAudio = gameObject.AddComponent<AudioSource> ();
-				mapAudio.clip = sound;
-
-				Select (buttons [0]);
+			b.AddComponent<GUIText>();
+			b.guiText.text = b.GetComponent<MapButton>().levelNameDisplay;
+			b.guiText.font = font;
+			b.guiText.fontSize = 55;
+			b.guiText.pixelOffset = new Vector2 (0, 0);
 		}
 
-		void OnGUI ()
+		//TODO what do i do with this?
+		mapAudio = gameObject.AddComponent<AudioSource> ();
+		mapAudio.clip = sound;
+
+		Select (buttons[0]);
+	}
+
+	void OnGUI ()
+	{
+		// scale the GUI to the current screen size
+		GUI.matrix = Globals.PrepareMatrix ();
+
+		// set the GUI images and font
+		GUI.skin.font = font;
+		GUI.skin.GetStyle ("Label").fontSize = 72;
+		GUI.skin.GetStyle ("Label").alignment = TextAnchor.MiddleLeft;
+		foreach (GameObject b in buttons)
 		{
-				// scale the GUI to the current screen size
-				GUI.matrix = Globals.PrepareMatrix ();
+			GUI.Label(new Rect (Globals.originalWidth/2 + b.transform.position.x, b.transform.position.y, 500, 500), b.GetComponent<MapButton>().levelNameDisplay);
+		}
 
-				// set the GUI images and font
-				GUI.skin.font = font;
-				GUI.skin.GetStyle ("Label").fontSize = 72;
-				GUI.skin.GetStyle ("Label").alignment = TextAnchor.LowerCenter;
+		// set the GUI images and font
+		GUI.skin.font = font;
+		GUI.skin.GetStyle ("Label").fontSize = 72;
+		GUI.skin.GetStyle ("Label").alignment = TextAnchor.LowerCenter;
 
-				if (anchor == Directions.Left) {
-						int groupWidth = 600;
-						int leftX = 0;
-						GUI.BeginGroup (new Rect (leftX, 0, groupWidth, Globals.originalHeight));
-						GUI.Box (new Rect (0, 0, groupWidth, Globals.originalHeight), guiImage);
-						GUI.Label (new Rect (0, 0, groupWidth, 100), levelName);
+		int groupWidth = 600;
+		int leftX = 0;
+		GUI.BeginGroup (new Rect (leftX, 0, groupWidth, Globals.originalHeight));
+		GUI.Box (new Rect (0, 0, groupWidth, Globals.originalHeight), guiImage);
+		GUI.Label (new Rect (0, 0, groupWidth, 100), levelName);
 
-						startButton2.x = leftX + 300;
-						startButton2.y = 300;
-						backButton.x = leftX + 300;
-						backButton.y = 800;
+		startButton.x = leftX + 300;
+		startButton.y = 300;
+		backButton.x = leftX + 300;
+		backButton.y = 800;
 
-						GUI.EndGroup ();
+		GUI.EndGroup ();
+
+		// reset the resolution
+		GUI.matrix = Matrix4x4.identity;
+	}
+
+	void Update ()
+	{
+		// check for input
+		if (Input.GetMouseButtonDown (0))
+		{
+			RaycastHit hit;
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+
+			if (Physics.Raycast (ray, out hit, 100.0f))
+			{
+				GameObject hitButton = hit.transform.gameObject;
+				if (hitButton.CompareTag ("MapButtonTag") && !hitButton.GetComponent<MapButton> ().locked)
+				{
+					Select (hit.transform.gameObject);
+					mapAudio.Play ();
 				}
-
-				// reset the resolution
-				GUI.matrix = Matrix4x4.identity;
+			}
 		}
-
-		void Update ()
+		else
 		{
-				if (Input.GetMouseButtonDown (0)) {
-						RaycastHit hit;
-						Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			if (Input.GetAxisRaw ("Horizontal") != 0)
+			{
+				if (!axisBusy)
+				{
+					axisBusy = true;
+					if (++selected > buttons.Length - 1 || buttons [selected].GetComponent<MapButton> ().locked)
+					{
+						selected = 0;
+					}
 
-						if (Physics.Raycast (ray, out hit, 100.0f)) {
-								GameObject hitButton = hit.transform.gameObject;
-								if (hitButton.CompareTag ("MapButtonTag") && !hitButton.GetComponent<MapButton> ().locked) {
-										Select (hit.transform.gameObject);
-										mapAudio.Play ();
-								}
-						}
-				} else {
-						if (Input.GetAxisRaw ("Horizontal") != 0) {
-								if (!axisBusy) {
-										axisBusy = true;
-										if (++selected > buttons.Length - 1 || buttons [selected].GetComponent<MapButton> ().locked) {
-												selected = 0;
-										}
-
-										Select (buttons [selected]);
-										mapAudio.PlayOneShot (sound);
-								}
-						}
-						if (Input.GetAxisRaw ("Horizontal") == 0) {
-								axisBusy = false;
-						}
+					Select (buttons [selected]);
+					mapAudio.PlayOneShot (sound);
 				}
+			}
+			if (Input.GetAxisRaw ("Horizontal") == 0)
+			{
+					axisBusy = false;
+			}
 		}
+	}
 
-		void Select (GameObject selectedButton)
+	void Select (GameObject selectedButton)
+	{
+		UnselectAll ();
+
+		// do the special stuff for the selected button
+		if (!selectedButton.GetComponent ("Halo"))
 		{
-				UnselectAll ();
-
-				if (!selectedButton.GetComponent ("Halo"))
-						selectedButton.transform.gameObject.AddComponent ("Halo");
-
-				selected = (int)selectedButton.GetComponent<MapButton> ().Scene;
-
-				startButton.GetComponent<GUIButton> ().scene = (SceneEnum)selected;
-
-				levelName = selectedButton.GetComponent<MapButton> ().levelNameDisplay;
+			selectedButton.transform.gameObject.AddComponent ("Halo");
 		}
 
-		void UnselectAll ()
+		selected = (int)selectedButton.GetComponent<MapButton> ().Scene;
+
+		startButton.GetComponent<GUIButton> ().scene = (SceneEnum)selected;
+
+		levelName = selectedButton.GetComponent<MapButton> ().levelNameDisplay;
+	}
+
+	void UnselectAll ()
+	{
+		foreach (GameObject button in buttons)
 		{
-				foreach (GameObject button in buttons) {
-						if (button.GetComponent ("Halo"))
-								Destroy (button.GetComponent ("Halo"));
-				}
+			if (button.GetComponent ("Halo"))
+			{
+				Destroy (button.GetComponent ("Halo"));
+			}
 		}
+	}
 }

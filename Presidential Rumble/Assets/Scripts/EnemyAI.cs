29f39156612cbcf9494eb3fa3434466148cb25b6
donald93 <blargalicious@gmail.com>
2014-03-25 12:40:52 -0600

    Pauses the fight until user presses start on the GUI

--- Presidential Rumble/Assets/Scripts/EnemyAI.cs
+++ Presidential Rumble/Assets/Scripts/EnemyAI.cs
@@ -40,59 +40,63 @@ public class EnemyAI : MonoBehaviour
 		// Update is called once per frame
 		void Update ()
 		{
-				grounded = Physics2D.Linecast (transform.position, groundCheck.position, 1 << LayerMask.NameToLayer ("Ground"));
+				if (!Globals.paused) {
+						grounded = Physics2D.Linecast (transform.position, groundCheck.position, 1 << LayerMask.NameToLayer ("Ground"));
 
-				if (!attacking && transform.position.x > 
-						GameObject.FindWithTag ("Player").GetComponent<PlayerController> ().getX () && transform.localScale.x > 0) {
-						Vector2 scale = transform.localScale;
-						scale.x *= -1;
-						transform.localScale = scale;
-				} else if (!attacking && transform.position.x < GameObject.FindWithTag ("Player").GetComponent<PlayerController> ().getX () && transform.localScale.x < 0) {
-						Vector2 scale = transform.localScale;
-						scale.x *= -1;
-						transform.localScale = scale;
+						if (!attacking && transform.position.x > 
+								GameObject.FindWithTag ("Player").GetComponent<PlayerController> ().getX () && transform.localScale.x > 0) {
+								Vector2 scale = transform.localScale;
+								scale.x *= -1;
+								transform.localScale = scale;
+						} else if (!attacking && transform.position.x < GameObject.FindWithTag ("Player").GetComponent<PlayerController> ().getX () && transform.localScale.x < 0) {
+								Vector2 scale = transform.localScale;
+								scale.x *= -1;
+								transform.localScale = scale;
+						}
 				}
 
 		}
 
 		void FixedUpdate ()
 		{
-				if (Mathf.Abs (rigidbody2D.transform.position.x - player.transform.position.x) < 3) {
-						moveHorizontal = 0;
-						rigidbody2D.velocity = Vector2.zero;
+				if (!Globals.paused) {
+						if (Mathf.Abs (rigidbody2D.transform.position.x - player.transform.position.x) < 3) {
+								moveHorizontal = 0;
+								rigidbody2D.velocity = Vector2.zero;
 			
-						if (cooldown == 0 && animator != null) {
-								int rand = Random.Range (0, 2);
-								if (rand == 1) {
-										attacking = true;
-										punch = true;
-										Punch ();
-								} else {
-										attacking = true;
-										kick = true;
-										Kick ();
+								if (cooldown == 0 && animator != null) {
+										int rand = Random.Range (0, 2);
+										if (rand == 1) {
+												attacking = true;
+												punch = true;
+												Punch ();
+										} else {
+												attacking = true;
+												kick = true;
+												Kick ();
+										}
 								}
-						}
 								
-				} else {
-						int rand = Random.Range (0, 2);
-						if (rand == 1)
-								MoveTowardsPlayer ();
-						else
-								Invoke ("MoveTowardsPlayer", 1f);
-				}	
-				if (jump) {
-						rigidbody2D.AddForce (new Vector2 (0f, jumpForce));	
-						jump = false;
-						jumping = false;
-				}
+						} else {
+								int rand = Random.Range (0, 2);
+								if (rand == 1)
+										MoveTowardsPlayer ();
+								else
+										Invoke ("MoveTowardsPlayer", 1f);
+						}	
+						if (jump) {
+								rigidbody2D.AddForce (new Vector2 (0f, jumpForce));	
+								jump = false;
+								jumping = false;
+						}
 		
-				framesSinceJump++;	
+						framesSinceJump++;	
 
-				if (grounded && (framesSinceJump > 0)) {
-						animator.SetBool ("Jumping", false);
-						jumping = false;
-						jump = false;
+						if (grounded && (framesSinceJump > 0)) {
+								animator.SetBool ("Jumping", false);
+								jumping = false;
+								jump = false;
+						}
 				}
 		}
 
--- Presidential Rumble/Assets/Scripts/PlayerController.cs
+++ Presidential Rumble/Assets/Scripts/PlayerController.cs
@@ -30,86 +30,89 @@ public class PlayerController : MonoBehaviour
 
 		void Update ()
 		{
-				grounded = Physics2D.Linecast (transform.position, groundCheck.position, 1 << LayerMask.NameToLayer ("Ground"));
-
-				if (animator) {
-
-						//Crouching controls
-						if (Input.GetKeyDown ("s") || Input.GetKeyDown (KeyCode.Joystick1Button5)) {
 				
-								animator.SetBool ("Crouching", true);
-								crouch = true;
-						}
+				if (!Globals.paused) {
+						grounded = Physics2D.Linecast (transform.position, groundCheck.position, 1 << LayerMask.NameToLayer ("Ground"));
 
-						if (Input.GetKeyUp ("s") || Input.GetKeyUp (KeyCode.Joystick1Button5)) {
-								animator.SetBool ("Crouching", false);
-								crouch = false;
-						} 
+						if (animator) {
 
-						//Blocking controls
-						if (Input.GetKeyDown ("q") || Input.GetKeyDown (KeyCode.Joystick1Button4)) {
-								block = true;
-						}
+								//Crouching controls
+								if (Input.GetKeyDown ("s") || Input.GetKeyDown (KeyCode.Joystick1Button5)) {
+				
+										animator.SetBool ("Crouching", true);
+										crouch = true;
+								}
 
-						if (Input.GetKeyUp ("q") || Input.GetKeyDown (KeyCode.Joystick1Button4)) {
-								block = false;
-						}
+								if (Input.GetKeyUp ("s") || Input.GetKeyUp (KeyCode.Joystick1Button5)) {
+										animator.SetBool ("Crouching", false);
+										crouch = false;
+								} 
 
-						// Jump Controls
-						if ((Input.GetKeyDown ("space") || Input.GetKeyDown (KeyCode.Joystick1Button0) || Input.GetKeyDown (KeyCode.Joystick1Button3)) && grounded) {
-								jump = true;
-								jumping = true;
-								block = false;
-								animator.SetBool ("Jumping", true);
-								framesSinceJump = 0;
-								audio.PlayOneShot (jumpSound);
-						}
+								//Blocking controls
+								if (Input.GetKeyDown ("q") || Input.GetKeyDown (KeyCode.Joystick1Button4)) {
+										block = true;
+								}
+
+								if (Input.GetKeyUp ("q") || Input.GetKeyDown (KeyCode.Joystick1Button4)) {
+										block = false;
+								}
+
+								// Jump Controls
+								if ((Input.GetKeyDown ("space") || Input.GetKeyDown (KeyCode.Joystick1Button0) || Input.GetKeyDown (KeyCode.Joystick1Button3)) && grounded) {
+										jump = true;
+										jumping = true;
+										block = false;
+										animator.SetBool ("Jumping", true);
+										framesSinceJump = 0;
+										audio.PlayOneShot (jumpSound);
+								}
 			
-						if (grounded && framesSinceJump > 0) {
-								animator.SetBool ("Jumping", false);
-								jumping = false;
+								if (grounded && framesSinceJump > 0) {
+										animator.SetBool ("Jumping", false);
+										jumping = false;
+								}
+
+
+
+								// Punch key was pushed
+								if ((Input.GetKeyDown ("f") || Input.GetKeyDown (KeyCode.Joystick1Button1)) && !attacking) {
+										animator.SetBool ("Punching", true);
+										attacking = true;	
+										// loop through children and enable the punch colliders
+										Transform[] allChildren = GetComponentsInChildren<Transform> ();
+										foreach (Transform child in allChildren) {
+												if (child.tag == "Punch")
+														child.collider2D.enabled = true;
+												Invoke ("disablePunch", 0.2f);
+										}
+								} 
+
+								// Kick key was pushed
+								if ((Input.GetKeyDown ("v") || Input.GetKeyDown (KeyCode.Joystick1Button2)) && !attacking) {
+										attacking = true;				
+										animator.SetBool ("Kicking", true);
+										// loop through children and enable the punch colliders
+										Transform[] allChildren = GetComponentsInChildren<Transform> ();
+										foreach (Transform child in allChildren) {
+												if (child.tag == "Kick")
+														child.collider2D.enabled = true;
+												Invoke ("disableKick", 0.5f);
+										}
+								}
 						}
 
-
-
-						// Punch key was pushed
-						if ((Input.GetKeyDown ("f") || Input.GetKeyDown (KeyCode.Joystick1Button1)) && !attacking) {
-								animator.SetBool ("Punching", true);
-								attacking = true;	
-								// loop through children and enable the punch colliders
-								Transform[] allChildren = GetComponentsInChildren<Transform> ();
-								foreach (Transform child in allChildren) {
-										if (child.tag == "Punch")
-												child.collider2D.enabled = true;
-										Invoke ("disablePunch", 0.2f);
-								}
-						} 
-
-						// Kick key was pushed
-						if ((Input.GetKeyDown ("v") || Input.GetKeyDown (KeyCode.Joystick1Button2)) && !attacking) {
-								attacking = true;				
-								animator.SetBool ("Kicking", true);
-								// loop through children and enable the punch colliders
-								Transform[] allChildren = GetComponentsInChildren<Transform> ();
-								foreach (Transform child in allChildren) {
-										if (child.tag == "Kick")
-												child.collider2D.enabled = true;
-										Invoke ("disableKick", 0.5f);
-								}
+						if (!attacking && transform.position.x > 
+								GameObject.FindWithTag ("Enemy").GetComponent<EnemyCollisions> ().getX () && transform.localScale.x > 0) {
+								Vector2 scale = transform.localScale;
+								scale.x *= -1;
+								transform.localScale = scale;
+						} else if (!attacking && transform.position.x < GameObject.FindWithTag ("Enemy").GetComponent<EnemyCollisions> ().getX () && transform.localScale.x < 0) {
+								Vector2 scale = transform.localScale;
+								scale.x *= -1;
+								transform.localScale = scale;
 						}
-				}
 
-				if (!attacking && transform.position.x > 
-						GameObject.FindWithTag ("Enemy").GetComponent<EnemyCollisions> ().getX () && transform.localScale.x > 0) {
-						Vector2 scale = transform.localScale;
-						scale.x *= -1;
-						transform.localScale = scale;
-				} else if (!attacking && transform.position.x < GameObject.FindWithTag ("Enemy").GetComponent<EnemyCollisions> ().getX () && transform.localScale.x < 0) {
-						Vector2 scale = transform.localScale;
-						scale.x *= -1;
-						transform.localScale = scale;
 				}
-
 		}
 
 		// Called each update

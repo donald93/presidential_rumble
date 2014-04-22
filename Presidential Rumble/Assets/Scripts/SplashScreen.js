acb0837467d6fe1aeade71dd013d7223aab1a1c8
donald93 <blargalicious@gmail.com>
2014-04-22 13:16:48 -0600

    Added Bryan's screen as intro splash screen

---
+++ Presidential Rumble/Assets/Scripts/SplashScreen.js
@@ -0,0 +1,21 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Awake(){
+
+	Splash();
+}
+
+function Splash(){
+
+	yield WaitForSeconds(3.0);
+	Application.LoadLevel("MainMenu");
+
+}
+
+function Update () {
+
+}
